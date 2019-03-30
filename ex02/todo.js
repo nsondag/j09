7b4c6c222a340409746dfa951787fbd4ddc31a41
Luna <luna@Tchuuk.local>
2019-03-30 01:37:43 +0100

    ex02

--- ex02/todo.js
+++ ex02/todo.js
@@ -1,6 +1,17 @@
 function add() {
 	var task = prompt("Enter a new task", "");
-	var todo = document.getElementById('ft_list');
-	var new_todo = document.createElement('div');
-	new_todo.appendChild(task);
+	if (task) {
+		var task_node = document.createTextNode(task);
+		var my_todo = document.getElementById('ft_list');
+		var new_elem = document.createElement('div');
+		new_elem.appendChild(task_node);
+		my_todo.insertBefore(new_elem, my_todo.childNodes[0]);
+	var listItems = my_todo.getElementsByTagName("div");
+	for (var i = 0; i < listItems.length; i++) {
+ 		 listItems[i].onclick = function remove() {
+		 var check = confirm("Are you sure?");
+		 if (check)
+		 	this.parentNode.removeChild(this);}
+	}
+	}
 }
